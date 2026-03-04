f805f011d9a17f4b5c84396f8989d2bfbb935284
EchoCat <khikichi331@gmail.com>
2026-03-04 16:06:53 +0900

    add search functionality2

--- scraper/spider.py
+++ scraper/spider.py
@@ -1,24 +1,29 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from dotenv import load_dotenv
+import os
+from time import sleep
 
-
+load_dotenv()
+query = os.getenv('SEARCH_QUERY')
 TARGET_URL = 'https://www.google.com/maps'
 try:
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless=new')
+    # options.add_argument('--headless=new')
     driver = webdriver.Chrome(options=options)
     driver.get(TARGET_URL)
     print(f'Page title:{driver.title}')
     wait = WebDriverWait(driver,10)
 
     # find element
-    search_ob = wait.until(EC.element_to_be_clickable((By.CLASS_NAME,'UGojuc')))
-    search_ob.send_keys('dentist Texas')
+    search_ob = wait.until(EC.element_to_be_clickable((By.ID,'ucc-1')))
+    search_ob.send_keys(query)
+    sleep(2)
     search_ob.send_keys(Keys.ENTER)
-    
+    sleep(2)
+
 finally:
     driver.quit()
